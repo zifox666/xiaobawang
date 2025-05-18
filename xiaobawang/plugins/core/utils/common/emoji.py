3d7cd2529589abf3d7a5f4c775bd5278058a42e8
newdoublex <95174933+zifox666@users.noreply.github.com>
2025-05-18 22:22:49 +0800

    [CORE] :bug: 防止表情回应失败导致整体退出

--- xiaobawang/plugins/core/utils/common/emoji.py
+++ xiaobawang/plugins/core/utils/common/emoji.py
@@ -1,3 +1,4 @@
+from nonebot import logger
 from nonebot.adapters.onebot.v11 import GroupMessageEvent as ob11_GroupMessageEvent
 from nonebot.internal.adapter import Event
 from nonebot_plugin_alconna import message_reaction
@@ -9,8 +10,10 @@ async def emoji_action(event: Event, emoji: str = "🔥"):
     :param event: 事件对象
     :param emoji 除了ob11以外的emoji
     """
-
-    if isinstance(event, ob11_GroupMessageEvent):
-        await message_reaction(event=event, emoji="424")
-    else:
-        await message_reaction(event=event, emoji=emoji)
+    try:
+        if isinstance(event, ob11_GroupMessageEvent):
+            await message_reaction(event=event, emoji="12893")
+        else:
+            await message_reaction(event=event, emoji=emoji)
+    except Exception as e:
+        logger.error(f"表情包发送失败: {traceback.format_exc()}")
