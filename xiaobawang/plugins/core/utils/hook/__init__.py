11cfcb82fcdc3bf77ca7ba7dac9390e2d30fde89
newdoublex <95174933+zifox666@users.noreply.github.com>
2025-05-20 19:10:33 +0800

    [CORE] :bug: 修复被ruff移除的消息发送模块

--- xiaobawang/plugins/core/utils/hook/__init__.py
+++ xiaobawang/plugins/core/utils/hook/__init__.py
@@ -1,12 +1,30 @@
+from typing import Dict, Any
+
 from nonebot.adapters import Bot
+from nonebot.internal.adapter import Event
+from nonebot.message import event_preprocessor
+from nonebot_plugin_uninfo import Uninfo
+
 from ..common.cache import cache
 
 
 @Bot.on_calling_api
-async def handle_api_call(api: str,):
+async def handle_api_call(api: str, data: Dict[str, Any]):
     cache_key = "send_msg"
     if api == "send_msg":
         i = await cache.get(cache_key)
         i = int(i) + 1 if i else 1
         await cache.set(cache_key, str(i), -1)
+
+
+@event_preprocessor
+async def handle_event_preprocessor(
+        event: Event,
+        user_session: Uninfo
+):
+    if event.get_type() == "message":
+        session = event.get_session_id()
+        user_id = user_session.user.id
+        session_info = user_session.scene
+
 
